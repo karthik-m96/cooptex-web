17d6067a3f0b7830dcc7ded9d720be719157ef58
karthik-m96 <karthikm300796@gmail.com>
2023-05-12 16:37:16 +0530

    nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+      <div className="left"></div>
+      <div className="item">
+        <img src="img/eng.jpg" alt="" />
+        <KeyboardArrowDownIcon/>
+      </div>
+      <div className="center"></div>
+      <div className="right"></div>
+      </div>
+    </div>
   )
 }
 
