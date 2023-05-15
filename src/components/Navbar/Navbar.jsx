6867bb4e04f792222eb0b2ff67ccdf13bd3b5820
karthik-m96 <karthikm300796@gmail.com>
2023-05-15 13:39:34 +0530

    scss

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,50 +4,55 @@ import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        <div className="left"></div>
-        <div className="item">
-          <img src="img/eng.png" alt="" />
-          <KeyboardArrowDownIcon />
-        </div>
-        <div className="item">
-          <span>INR</span>
-          <KeyboardArrowDownIcon />
-        </div>
-        <div className="item">
-          <Link to="/products/1">Women</Link>
-        </div>
-        <div className="item">
-          <Link to="/products/2">Men</Link>
-        </div>
-        <div className="item">
-          <Link to="/products/3">Home Textiles</Link>
+        <div className="left">
+          <div className="item">
+            <img src="img/eng.png" alt="" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>INR</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Home Textiles</Link>
+          </div>
         </div>
         <div className="center">
           <Link to="/">Cooptex</Link>
         </div>
-        <div className="right">
+        <div className="center">
           <Link to="/">Homepage</Link>
         </div>
-        <div className="right">
+        <div className="center">
           <Link to="/">About</Link>
         </div>
-        <div className="right">
+        <div className="center">
           <Link to="/">Contact</Link>
         </div>
-        <div className="right">
+        <div className="center">
           <Link to="/">Stores</Link>
         </div>
         <div className="icons">
-          <SearchIcon/>
-          <PersonOutlineOutlinedIcon/>
-          <FavoriteBorderOutlinedIcon/>
-          <ShoppingCartOutlinedIcon/>
+          <SearchIcon />
+          <PersonOutlineOutlinedIcon />
+          <FavoriteBorderOutlinedIcon />
+          <div className="cartIcon">
+            <ShoppingCartOutlinedIcon />
+            <span>0</span>
+          </div>
         </div>
       </div>
     </div>
