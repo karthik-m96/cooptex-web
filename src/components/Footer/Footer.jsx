062961860fd355daee3a7939da5577d15821d1e3
karthik-m96 <karthikm300796@gmail.com>
2023-05-15 16:50:03 +0530

    footer sass

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import "./footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>ABOUT US</h1>
+          <span>About Us</span>
+          <span>Blog</span>
+          <span>Official Website</span>
+        </div>
+        <div className="item">
+          <h1>POLICIES</h1>
+          <span>Privacy Policy</span>
+          <span>Return & Exchange Policy</span>
+          <span>Shipping Policy</span>
+        </div>
+        <div className="item">
+          <h1>CONNECT WITH US</h1>
+          <span>Contact us</span>
+          <span>Disclaimer</span>
+          <span>Help/FAQ</span>
+          <span>Store Locator</span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span>The Tamilnadu Handloom Weavers Co-operative Society Ltd ©2022 All Right Reserved.</span>
+        </div>
+        <div className="right">
+          <img src="../img/pay.svg" alt="payment logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
