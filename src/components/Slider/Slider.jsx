891ea67eae02effac1606daaa20bebc7fc9c0b87
karthik-m96 <karthikm300796@gmail.com>
2023-05-17 11:16:43 +0530

    changes

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Slider = () => {
+
+  const data = [
+    "https://cooptex.gov.in/image/cache/data/Banner-/10copy-1920x900.jpg",
+    ""
+  ];
+
+  return (
+    <div className="slider">
+      <div className="container">
+        <img src="" alt="" className="data" />
+        <img src="" alt="" className="data" />
+        <img src="" alt="" className="data" />
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Slider from "../Slider/Slider.jsx"
+import "./Home.scss"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='slider'>
+      <Slider/>
+    </div>
   )
 }
 
