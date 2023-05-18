5bb1085388b09e31e37ad1cb9aabd51e57942b3b
karthik-m96 <karthikm300796@gmail.com>
2023-05-18 18:12:51 +0530

    sliderworking

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import "./FeaturedProducts.scss"
 
 const FeaturedProducts = () => {
+
   return (
-    <div>FeaturedProducts</div>
+    <div className='featuredProducts'>
+      <div className="top">
+        <h1>Products</h1>
+      </div>
+      <div className="bottom"></div>
+    </div>
   )
   }
 
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
-import "./Slider.scss";
+import "./Slider.scss"
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -9,15 +9,17 @@ const Slider = () => {
   const data = ["./img/banner1.jpg", "./img/banner2.jpg", "./img/banner3.jpg"];
 
   const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? 2 : (prev)  => prev - 1)
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
   };
   const nextSlide = () => {
-    setCurrentSlide(currentSlide === 2 ? 0 : (prev)  => prev + 1)
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
   };
-
   return (
     <div className="slider">
-      <div className="container" style={{transform:`translateX(-${currentSlide * 101}vw)`}}>
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
