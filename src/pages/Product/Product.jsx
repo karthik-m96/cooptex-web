b0c0db32a272338740b4287faefcbbe4c9c34112
karthik-m96 <karthikm300796@gmail.com>
2023-05-24 16:07:50 +0530

    card

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import "./Product.scss"
+import React from "react";
+import "./Product.scss";
 
 const Product = () => {
-  return (
-    <div className='product'>Product</div>
-  )
-}
+  const images = [
+    "https://images.pexels.com/photos/15985569/pexels-photo-15985569/free-photo-of-cold-snow-wood-light.jpeg",
+    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg",
+  ];
 
-export default Product
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Product;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -4,12 +4,11 @@ import List from "../../components/List/List";
 import { useParams } from "react-router-dom";
 
 const Products = () => {
-
-  const catId = parseInt(useParams().id)
+  const catId = parseInt(useParams().id);
 
   const [maxPrice, setMaxPrice] = useState(50000);
 
-  const [sort,setSort] = useState(null)
+  const [sort, setSort] = useState(null);
 
   return (
     <div className="products">
@@ -45,29 +44,47 @@ const Products = () => {
           <h2>Filter by price</h2>
           <div className="inputItem">
             <span>0</span>
-            <input type="range" min={0} max={50000} onChange={(e)=>setMaxPrice(e.target.value)}/>
+            <input
+              type="range"
+              min={0}
+              max={50000}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
             <span>{maxPrice}</span>
           </div>
         </div>
         <div className="filterItem">
           <h2>Sort by</h2>
           <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" onChange={(e) => setSort("asc")}/>
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort("asc")}
+            />
             <label htmlFor="asc">Price low to high</label>
           </div>
           <div className="inputItem">
-            <input type="radio" id="desc" value="desc" name="price" onChange={(e) => setSort("desc")}/>
+            <input
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort("desc")}
+            />
             <label htmlFor="desc">Price high to low</label>
           </div>
         </div>
       </div>
-      <div className="right"></div>
-      <img
-        className="catImg"
-        src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
-        alt="bannerimage"
-      />
-      <List catId={catId} maxPrice={maxPrice} sort={sort}/>
+      <div className="right">
+        <img
+          className="catImg"
+          src="/img/product.jpg"
+          alt="bannerimage"
+        />
+        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+      </div>
     </div>
   );
 };
