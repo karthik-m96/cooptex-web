f897d4bd04df47498a059bb660a625a8839c6749
karthik-m96 <karthikm300796@gmail.com>
2023-05-26 14:39:32 +0530

    stage

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -3,24 +3,14 @@ import "./Product.scss"
 
 const Product = () => {
   return (
-<<<<<<< HEAD
     <div className="product">
       <div className="left">
         <div className="images">
-          <div>
-            <img src={images[0]} alt="" />
-            <img src={images[1]} alt="" />
-          </div>
         </div>
       </div>
       <div className="right"></div>
     </div>
   );
 };
-=======
-    <div className='product'>Product</div>
-  )
-}
->>>>>>> parent of b0c0db3 (card)
 
 export default Product
