5b8b9eef11bdde1d6300a388771cb952f8fc8ce4
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-06-20 18:06:20 +0530

    slider timer

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
-import "./Slider.scss"
+import "./Slider.scss";
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const data = ["./img/banner1.jpg", "./img/banner2.jpg", "./img/banner3.jpg"];
-  
+
   const prevSlide = () => {
     setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
   };
   const nextSlide = () => {
     setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [currentSlide]);
+
   return (
     <div className="slider">
       <div
