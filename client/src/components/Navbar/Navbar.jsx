c211a907a2f110cac8d8216f07020d27ed43e52e
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-06-26 10:50:30 +0530

    update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
-import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
-import Cart from "../Cart/Cart"
+import Cart from "../Cart/Cart";
 
-const Navbar = () => { 
-
-  const [open, setOpen] = useState(false)
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="navbar">
       <div className="wrapper">
@@ -25,7 +21,10 @@ const Navbar = () => {
             </Link>
           </div>
           <div className="item">
-            <Link className="link" to="https://www.cooptex.gov.in/index.php?route=information/knt_scheme">
+            <Link
+              className="link"
+              to="https://www.cooptex.gov.in/index.php?route=information/knt_scheme"
+            >
               CMSS (Chit Scheme)
             </Link>
           </div>
@@ -59,14 +58,14 @@ const Navbar = () => {
             </Link>
           </div>
           <div className="item">
-            <Link className="link" to="https://cooptex.gov.in/index.php?route=information/store">
+            <Link
+              className="link"
+              to="https://cooptex.gov.in/index.php?route=information/store"
+            >
               Stores
             </Link>
           </div>
           <div className="icons">
-            <SearchIcon />
-            <PersonOutlineOutlinedIcon />
-            <FavoriteBorderOutlinedIcon />
             <div className="cartIcon" onClick={() => setOpen(!open)}>
               <ShoppingCartOutlinedIcon />
               <span>0</span>
