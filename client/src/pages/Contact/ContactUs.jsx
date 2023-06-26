a1c35d7b4b1dca6f376bbed8908a25a1e25abfcf
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-06-26 14:46:19 +0530

    responsive

--- client/src/pages/Contact/ContactUs.jsx
+++ client/src/pages/Contact/ContactUs.jsx
@@ -1,21 +1,21 @@
 import React from "react";
+import "./ContactUs.scss";
 
 const ContactUs = () => {
   return (
-    <div>
-      <div className="contact">
-        <h1>Contact us</h1>
-        <div className="content">
-          <h2>Co optex Online Shop</h2>
-          <h3>Address</h3>
-          <p>No. 350, Pantheon Road, Egmore, Chennai - 600 008</p>
-          <h3>Telephone</h3>
-          <p>044 28194760</p>
-          <h3>Toll Free Number</h3>
-          <p>1800-599-7633</p>
-          <h3>Opening Times</h3>
-          <p>Monday to Saturday : 10am to 7pm</p>
-        </div>
+    <div className="contactus">
+      <h1>Contact us</h1>
+      <div className="content">
+        <h2>Co optex Online Shop</h2>
+        <h3>Address:</h3>
+        <p>No. 350, Pantheon Road,</p>
+        <p>Egmore, Chennai - 600 008.</p>
+        <h3>Telephone:</h3>
+        <p>044-28194760.</p>
+        <h3>Toll Free Number:</h3>
+        <p>1800-599-7633.</p>
+        <h3>Opening Times:</h3>
+        <p>Monday to Saturday: 10 am to 7 pm.</p>
       </div>
     </div>
   );
