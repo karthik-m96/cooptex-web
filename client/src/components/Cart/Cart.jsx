74ba4ce79589a7e2392ef131c528900274bb36c1
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-03 12:26:26 +0530

    cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,26 +3,7 @@ import "./cart.scss";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 const Cart = () => {
-  const data = [
-    {
-      id: 1,
-      img: "https://www.cooptex.gov.in/image/cache/data/FEB/SKBSA/SKBSA/TRI3DSKBS-9silkset311sufisidetassels2023-2-21-18-13-331600X2400-285x318.jpg",
-      img2: "https://www.cooptex.gov.in/image/cache/data/FEB/SKBSA/SKBSA/TRI3DSKBS-9silkset309kavyaback12023-2-21-18-13-41600X2400-285x318.jpg",
-      title: "Kancheepuram Pure Zari Silk Sarees",
-      isNew: true,
-      oldPrice: "20,090",
-      price: "17,030",
-    },
-    {
-      id: 2,
-      img: "https://www.cooptex.gov.in/image/cache/data/FEB/SKBSA/SKBSA/TRI3DSKBS-1silkset311sufisidetassels2023-2-21-18-13-501600X2400-285x318.jpg",
-      img2: "https://www.cooptex.gov.in/image/cache/data/FEB/SKBSA/SKBSA/TRI3DSKBS-1silkset309kavyaback12023-2-21-18-13-481600X2400-285x318.jpg",
-      title: "Kancheepuram Pure Zari Silk Sarees",
-      isNew: true,
-      oldPrice: "21,028",
-      price: "17,834",
-    },
-  ];
+  
 
   return (
     <div className="cart">
