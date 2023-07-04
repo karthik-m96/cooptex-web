8af8c06abcc2ba921ec89a34b82571b9cfe6de20
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-04 16:09:41 +0530

    completed

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import "./cart.scss";
+import "./Cart.scss";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { useSelector } from "react-redux";
 import { removeItem, resetCart } from "../../redux/cartReducer";
 import { useDispatch } from "react-redux";
+import { makeRequest } from "../../makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const products = useSelector((state) => state.cart.products);
@@ -11,36 +13,55 @@ const Cart = () => {
 
   const totalPrice = () => {
     let total = 0;
-    products?.forEach((item) => (total += item.quantity * item.price));
+    products.forEach((item) => {
+      total += item.quantity * item.price;
+    });
     return total.toFixed(2);
   };
 
+  const stripePromise = loadStripe(
+    "pk_test_51NQ1aiSJyeHR8y5pUhzg6TvH4ecQVenkHBSnjcSSkpCimutt33arWuPI0XikqiTkWaQGZ5KxoN45eSWm8JLDmD4A00QGs445jb"
+  );
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="cart">
       <h1>Products in your cart</h1>
-      {products?.map((item) => {
-        return (
-          <div className="item" key={item.id}>
-            <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
-            <div className="details">
-              <h1>{item.title}</h1>
-              <div className="price">
-                {item.quantity} x Rs.{item.price}
-              </div>
+      {products?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <div className="price">
+              {item.quantity} x Rs.{item.price}
             </div>
-            <DeleteOutlinedIcon
-              className="delete"
-              onClick={() => dispatch(removeItem(item.id))}
-            />
           </div>
-        );
-      })}
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => dispatch(removeItem(item.id))}
+          />
+        </div>
+      ))}
       <div className="total">
-        <span>Subtotal</span>
-        <span>Rs.{totalPrice()}</span>
+        <span>SUBTOTAL</span>
+        <span>${totalPrice()}</span>
       </div>
-      <button>PROCEED TO CHECKOUT</button>
-      <span className="reset" onClick={() => dispatch(resetCart())}>Reset Cart</span>
+      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Reset Cart
+      </span>
     </div>
   );
 };
